e16121ec4dd98a8e1439cd97411a4994c6726fbe
BigKumpel <100943901+BigKumpel@users.noreply.github.com>
2022-06-10 04:15:46 +0200

    first commit

--- main.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector("#cookie-container");
-const btn = document.querySelector("#cookie-btn");
-
-btn.addEventListener("click", function display() {
-document.getElementById("cookie-container").style.display = "none";
-});
